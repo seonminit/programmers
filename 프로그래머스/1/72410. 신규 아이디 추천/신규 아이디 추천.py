375612076e57f3927dadc6b270407d191baaa1b1
seonminkim <108780544+seonminit@users.noreply.github.com>
2024-04-12 15:02:10 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.23 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -7,8 +7,7 @@ def solution(new_id):
     # 2
     new_id = re.sub("[^a-z0-9-_.]", "", new_id)
     # 3
-    while '..' in new_id:
-        new_id = new_id.replace('..', '.')
+    new_id = re.sub('[.]+', '.', new_id)
     # 4
     if (len(new_id) != 0 and new_id[0] == '.'):
         new_id = new_id[1:]
@@ -20,8 +19,7 @@ def solution(new_id):
     # 6
     elif len(new_id) > 15:
         new_id = new_id[:15]
-        if new_id[-1] == '.':
-            new_id = new_id[:-1]
+        new_id = re.sub('[.]$', '', new_id)
     else:
         pass
     # 7
