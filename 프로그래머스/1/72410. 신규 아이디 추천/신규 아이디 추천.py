17f71f1254e4057bc99723b4261fa13f8f05e631
seonminkim <108780544+seonminit@users.noreply.github.com>
2024-04-12 14:16:37 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.16 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -0,0 +1,33 @@
+import re 
+
+def solution(new_id):
+
+    # 1
+    new_id = new_id.lower()
+    # 2
+    new_id = re.sub("[^a-z0-9-_.]", "", new_id)
+    # 3
+    while '..' in new_id:
+        new_id = new_id.replace('..', '.')
+    # 4
+    if (len(new_id) != 0 and new_id[0] == '.'):
+        new_id = new_id[1:]
+    if (len(new_id) != 0 and new_id[-1] == '.'):
+        new_id = new_id[:-1]
+    # 5
+    if len(new_id) == 0:
+        new_id = "a"
+    # 6
+    elif len(new_id) > 15:
+        new_id = new_id[:15]
+        if new_id[-1] == '.':
+            new_id = new_id[:-1]
+    else:
+        pass
+    # 7
+    while len(new_id) < 3:
+        new_id = new_id + new_id[-1]
+    
+    answer = new_id
+
+    return answer
