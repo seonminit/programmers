dccdef1e9a9ae262c5efb0b53bd7ec2c6c6201ff
seonminkim <108780544+seonminit@users.noreply.github.com>
2024-04-14 14:07:10 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.25 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -0,0 +1,50 @@
+def solution(today, terms, privacies):
+    answer = []
+    # today
+    year, month, day = [int(item) for item in today.split('.')]
+    print('today:',year,month,day)
+    # terms
+    terms_dict = {}
+    for term in terms:
+        key, value = term.split(' ')
+        terms_dict[key] = int(value) 
+    print(terms_dict)
+    
+    # privacies
+    idx = 0
+    for privacy in privacies:
+        idx += 1
+        key = privacy[-1]
+        y, m, d = [int(item) for item in privacy[:-2].split('.')]
+        
+        target_y = y
+        target_m = m+terms_dict[key]
+        target_d = d-1
+        # 25 -> 2y, 1m
+        while target_m > 12:
+            target_m -= 12
+            target_y += 1
+            
+        if target_d == 0:
+            target_m -= 1
+            target_d = 28
+        
+        # check
+        if year < target_y:
+            continue
+        elif year == target_y:
+            if month < target_m:
+                continue
+            elif month == target_m:
+                if day <= target_d:
+                    continue 
+                else:
+                    answer.append(idx)
+            else:
+                answer.append(idx)       
+        else:
+            answer.append(idx)
+        
+        print(target_y, target_m, target_d)
+    print(answer)
+    return answer
