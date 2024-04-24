bdfb5fb1e2ac6fcfea06d0488320153c68478f54
seonminkim <108780544+seonminit@users.noreply.github.com>
2024-04-24 16:55:58 +0900

    [level 2] Title: 오픈채팅방, Time: 70.04 ms, Memory: 39.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
@@ -0,0 +1,26 @@
+def solution(record):
+    answer = []
+    
+    # setting_user
+    user_dict = {}
+    
+    answer_record = []
+    
+    for rec in record:
+        split_rec = rec.split(' ')
+        
+        if (split_rec[0] == 'Enter' or split_rec[0] == 'Leave'):
+            answer_record.append(rec)
+        # setting_user
+        if split_rec[0] == 'Leave':
+            continue
+        else:
+            user_dict[split_rec[1]] = split_rec[2]
+    
+    for rec in answer_record:
+        rec_split = rec.split(' ')
+        if rec_split[0] == 'Enter':
+            answer.append(f'{user_dict[rec_split[1]]}님이 들어왔습니다.')
+        else: 
+            answer.append(f'{user_dict[rec_split[1]]}님이 나갔습니다.')
+    return answer
