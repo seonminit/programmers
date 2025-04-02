8d07747a18d2ea381235b9ffc0b9522ae21b65db
seonminkim <108780544+seonminit@users.noreply.github.com>
2025-04-03 00:17:14 +0900

    [level 2] Title: 서버 증설 횟수, Time: 0.02 ms, Memory: 9.23 MB -BaekjoonHub

---
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.py
@@ -0,0 +1,21 @@
+def solution(players, m, k):
+    answer = 0
+    server_list = [0]*24
+
+    for idx in range(len(players)):
+        player = players[idx]
+        check = player // m
+        if check == 0:
+            pass
+        elif (check >= 1) and (check <= server_list[idx]):
+            pass
+        elif (check >= 1) and (check > server_list[idx]):
+            target_add = check - server_list[idx]
+            answer += target_add
+            for i in range(k):     
+                if idx+i <24:
+                    server_list[idx+i] += target_add
+                else: 
+                    pass     
+            
+    return answer
